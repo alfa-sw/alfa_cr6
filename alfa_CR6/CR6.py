00e6d258108242a54ce3b21851560448505bbc85
Gaber Ayoubi <gaber.ayoubi@freelands.it>
2020-10-08 11:51:52 +0200

    fix setup.py

--- alfa_CR6/CR6.py
+++ alfa_CR6/CR6.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python3
 import sys
+import os
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton
 from PyQt5 import QtWidgets, uic
@@ -6,12 +8,14 @@ from PyQt5.Qt import *
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtWidgets import QApplication, QPushButton
 
+PATH=os.path.dirname(os.path.abspath(__file__))
+
 class Window(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        loadUi("mainwindow.ui", self)
-        self.frame.setStyleSheet("background-image: url('machine.jpg'); background-repeat: no-repeat; background-position: center;")
+        loadUi(PATH+"/ui/mainwindow.ui", self)
+        self.frame.setStyleSheet("background-image: url('"+PATH+"/ui/machine.jpg'); background-repeat: no-repeat; background-position: center;")
         self.dialog_1_Btn.clicked.connect(self.onDialog1BtnClicked)
         self.chrome_Btn.clicked.connect(self.onChromeBtnClicked)
 
@@ -28,22 +32,24 @@ class Window(QMainWindow):
 class Dialog1(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
-        loadUi("dialog.ui", self)
+        loadUi(PATH+"/ui/dialog.ui", self)
 
 class ChromeDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
-        loadUi("chrome.ui", self)
+        loadUi(PATH+"/ui/chrome.ui", self)
         view = QWebEngineView(self)
         view.setUrl(QUrl("http://www.google.com"))
         view.resize(1024, 650)
         view.show()
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    win = Window()
-    win.show()
+class Cr6():
+#if __name__ == "__main__":
+    def __init__(self, parent=None):
+        app = QApplication(sys.argv)
+        win = Window()
+        win.show()
 
-    sys.exit(app.exec())
+        sys.exit(app.exec())
 
 
--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
+import glob
 
 setup (
     name='alfa_CR6',
     version='1.0',
     description='ALFA CR& Project',
     packages=['alfa_CR6'],
-    install_requires=[
-        'alfa_CR6'
-    ],
-    scripts[
-    ]
+    #modules_py=['alfa_CR6', 'alfa_cr6.CR6'],
+    package_data={'alfa_CR6/ui': glob.glob('alfa_CR6/ui/*')},
+    data_files=[('alfa_CR6/ui', list(glob.glob('alfa_CR6/ui/*')))],
+   # install_requires=[
+   #     'alfa_CR6'
+   # ],
+    scripts=['alfa_CR6/scripts/cr6']
+    
 
 )
