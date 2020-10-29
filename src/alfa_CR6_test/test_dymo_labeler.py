82ad373b59e0fafa2b28eb460954a4b4829f7cc7
FrancescoGalasso <francescogalasso@alfadispenser.com>
2020-10-29 09:46:00 +0100

    improved test_dymo_labeler

--- src/alfa_CR6_test/test_dymo_labeler.py
+++ src/alfa_CR6_test/test_dymo_labeler.py
@@ -14,91 +14,53 @@ import traceback
 import logging
 import subprocess
 import argparse
-from PIL import Image
-from PIL import ImageFont
-from PIL import ImageDraw
 from barcode import EAN13                   # pylint: disable=import-error
 from barcode.writer import ImageWriter      # pylint: disable=import-error
 
 
-
-#
-#	TO RUN THIS TEST IS NECESSARY INSTALL THOSE PIP PKG INTO VENV:
-#   pip install python-barcode
-#   pip install Pillow
-#
-
-HERE = pathlib.Path().absolute()
-# PATH_TMP_FILE = os.path.join(str(HERE), os.path.sep, 'tmp_file.jpg')
-# PATH_COMPLETE_FILE = os.path.join(str(HERE), os.path.sep, 'label.png')
-PATH_TMP_FILE = str(HERE) + os.path.sep + 'tmp_file.png'
-PATH_COMPLETE_FILE = str(HERE) + os.path.sep + 'label.png'
-
-
-DEFAULT_TEST_SETTINGS = {'ean13': '201027001001',
-                         'label_details': 'LY9C VOLKSWAGEN-AUDI\nA4',
-                         'printer_model': 'Dymo',}
+HERE = os.path.dirname(os.path.abspath(__file__))
+PATH_TMP_FILE = HERE + os.path.sep + 'tmp_file.png'
+DEFAULT_TEST_SETTINGS = {'recipe_barcode': '201027001001',
+                         'printer_model': 'Dymo',
+                         'recipe_color_code': 'LY9C',
+                         'recipe_manufacturer': 'VOLKSWAGEN-AUDI',
+                         'recipe_car_model': 'A4',
+                         'recipe_qty': '1 L'}
 
 
 def _create_printable_image():
-    """create a printable image .png for DYMO 450 Turbo
-            1 step - create barcode img
-            2 step - create a new img adding inside of it the one created at step 1
-            3 step - add text contents in the img created at step 2"""
+    """ create a printable image .png for DYMO 450 Turbo """
 
     response = {}
     try:
-        # STEP 1
-        print(PATH_TMP_FILE)
+
         if not os.path.exists(PATH_TMP_FILE):
             with open(PATH_TMP_FILE, 'w'):
                 pass
 
         with open(PATH_TMP_FILE, 'wb') as file_:
-            barcode_img = EAN13(DEFAULT_TEST_SETTINGS.get('ean13'), writer=ImageWriter())
-            #options = {
-             #    'dpi': 100,
-             #   # 'module_height': 5,
-             #   # 'quiet_zone': 0,
-             #   'font_size': 24,
-             #   'text_distance': 0.75,
-             #   #'compress':True,
-            #}
+            barcode_img = EAN13(DEFAULT_TEST_SETTINGS.get('recipe_barcode'), writer=ImageWriter())
+
             options = {
-                'dpi': 100,
-                # 'module_height': 5,
+                'dpi': 250,
+                'module_height': 7,
                 # 'quiet_zone': 0,
-                'font_size': 6,
+                'font_size': 15,
                 'text_distance': 0.75,
-                'compress':True,
+                'compress':False,
                 }
-            barcode_img.write(file_, options)
 
-        # STEP 2
-        img = Image.open(PATH_TMP_FILE, 'r')
-        img_w, img_h = img.size
-        # img = img.resize((523, int(img_h * .65)), Image.ANTIALIAS)
-        img = img.resize((523, int(img_h * .65)))
-        background = Image.new('RGBA', (600, 300), (255, 255, 255, 255))
-        bg_w, bg_h = background.size
-        offset = ((bg_w - img_w) // 2, ((bg_h - img_h) // 2) + 80)
-        background.paste(img, offset)
-        background.save(PATH_COMPLETE_FILE, optimize=True, quality=20)
-        if os.path.exists(PATH_TMP_FILE):
-            os.remove(PATH_TMP_FILE)
+            logging.debug('barcode_img({}) -> {}'.format(type(barcode_img), barcode_img))
 
-        # STEP 3
-        img = Image.open(PATH_COMPLETE_FILE)
-        draw = ImageDraw.Draw(img)
-        # TODO: in future get font from folder_font
-        font_path = '/usr/share/fonts/truetype/ubuntu/Ubuntu-B.ttf'
-        if not os.path.exists(font_path):
-            font_path = '/usr/share/fonts/truetype/dejavu/DejaVuSansMono-Bold.ttf'
-        font = ImageFont.truetype(font_path, 40)
-        draw.text((40, 10), DEFAULT_TEST_SETTINGS.get('label_details'), "black", font=font)
-        img.save(PATH_COMPLETE_FILE, optimize=True, quality=20)
-        logging.info('Image created at {}'.format(PATH_COMPLETE_FILE))
-        response = {'result': 'OK', 'file': PATH_COMPLETE_FILE}
+            custom_text = '{}\n{} {}\n{}\n{}'.format(DEFAULT_TEST_SETTINGS.get('recipe_barcode'),
+                                                     DEFAULT_TEST_SETTINGS.get('recipe_color_code'),
+                                                     DEFAULT_TEST_SETTINGS.get('recipe_manufacturer'),
+                                                     DEFAULT_TEST_SETTINGS.get('recipe_car_model'),
+                                                     DEFAULT_TEST_SETTINGS.get('recipe_qty'),)
+            barcode_img.write(file_, options, custom_text)
+
+        logging.info('Image created at {}'.format(PATH_TMP_FILE))
+        response = {'result': 'OK', 'file': PATH_TMP_FILE}
     except Exception:   # pylint: disable=broad-except
         logging.error(traceback.format_exc())
         response = {'result': 'KO', 'error': traceback.format_exc()}
@@ -126,7 +88,7 @@ def send_cups_cmd(action, params):
 
     if action == 'cups_print_label':
         _path = params.get('file')
-        _res = _format_reply('lp -o media=Custom.20x80mm {}'.format(_path))
+        _res = _format_reply('lp -o fit-to-page {}'.format(_path))
         response.update({'result': 'OK', 'data': _res[0]})
     elif action == 'cups_check_printer_presence':
         # really really slow :-/
