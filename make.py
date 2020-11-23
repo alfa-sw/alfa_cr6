a5a8815967403f8dbc511d9263ea523ba1a7ccaf
giovanni-angeli <giovanniangeli@alfadispenser.com>
2020-11-23 15:49:49 +0100

    make.py tool development

--- make.py
+++ make.py
@@ -22,6 +22,7 @@ DATA_PATH = f"{DEPLOY_PATH}/data"
 VAR_PATH = f"{DEPLOY_PATH}/var"
 TMP_PATH = f"{DEPLOY_PATH}/tmp"
 
+OUT_ERR_PTH = f"{PROJECT_ROOT}/make.err.out"
 
 def parse_arguments():
 
@@ -32,7 +33,7 @@ def parse_arguments():
         default='INFO',
         help='level of verbosity in logging messages default: %(default)s.')
     parser.add_argument('-t', '--target_credentials', default='admin@192.168.1.100', help='default: "%(default)s".')
-    parser.add_argument('-s', '--app_settings', default='app_settings.production.py', help='default: "%(default)s".')
+    parser.add_argument('-s', '--app_settings', default='app_settings.production', help='default: "%(default)s".')
     parser.add_argument('-i', '--ignore_requires', help='if not None, ignore_requires in installing. default: "%(default)s".', action='store_const', const=1)
     parser.add_argument('-d', '--dry_run', action='store_const', const=True,
                         help='dry run: if not None, just test, do nothing. default: "%(default)s".')
@@ -40,6 +41,7 @@ def parse_arguments():
     parser.add_argument('-b', '--build', help='action: build the wheel.', action='store_const', const=1)
     parser.add_argument('-e', '--install_editable', action='store_const', const=1)
     parser.add_argument('-I', '--install_target', action='store_const', const=1)
+    parser.add_argument('-M', '--makedirs_on_target', action='store_const', const=1)
     parser.add_argument('-D', '--deploy_db_to_target', action='store_const', const=1,
                         help='action: copy the sqlite db to target, *BEWARE* overwriting it!')
 
@@ -49,7 +51,8 @@ def parse_arguments():
 def exec_(cmd_, dry):
     logging.info(f"dry:{dry} cmd_:{cmd_}")
     if not dry:
-        os.system(cmd_ + f" > {PROJECT_ROOT}/make.log")
+        ret_val = os.system(cmd_ + f"  >>{OUT_ERR_PTH} 2>&1 ")
+        logging.info(f"ret_val:{ret_val}")
 
 
 def build(args):
@@ -68,23 +71,30 @@ def deploy_db_to_target(args):
     exec_(cmd_, dry=args.dry_run)
 
 
-def install_target(args):
+def makedirs_on_target(args):
 
     tgt_cred = args.target_credentials
-    settings = args.app_settings
-    ignore_requires = '--ignore-requires --no-dependencies ' if args.ignore_requires else ''
 
     for pth in (VENV_PATH, CONF_PATH, LOG_PATH, DATA_PATH, VAR_PATH, TMP_PATH):
         cmd_ = f'ssh {tgt_cred} "if [ ! -e {pth} ]; then mkdir -p {pth} ;fi"'
         exec_(cmd_, dry=args.dry_run)
         cmd_ = f'ssh {tgt_cred} "sudo chmod -R a+rw {pth}"'
         exec_(cmd_, dry=args.dry_run)
-        # ~ time.sleep(.1)
+
+def install_target(args):
+
+    tgt_cred = args.target_credentials
+    settings = args.app_settings
+    ignore_requires = '--ignore-requires --no-dependencies ' if args.ignore_requires else ''
 
     cmds = [
         f"scp {PROJECT_ROOT}/dist/alfa_CR6-{__version__}-py3-none-any.whl {tgt_cred}:{TMP_PATH}/",
-        f"scp {PROJECT_ROOT}/conf/{settings}.py {tgt_cred}:{CONF_PATH}/app_settings.py",
         f'ssh {tgt_cred} ". /opt/alfa_cr6/venv/bin/activate; pip install {ignore_requires} {TMP_PATH}/alfa_CR6-{__version__}-py3-none-any.whl"',
+        f"scp {PROJECT_ROOT}/conf/{settings}.py {tgt_cred}:{CONF_PATH}/app_settings.py",
+        f"scp {PROJECT_ROOT}/conf/cr6.desktop {tgt_cred}:{TMP_PATH}/cr6.desktop",
+        f'ssh {tgt_cred} "sudo cp {TMP_PATH}/cr6.desktop /etc/xdg/autostart/cr6.desktop"',
+        f'ssh {tgt_cred} "sudo killall /opt/alfa_cr6/venv/bin/python"',
+        f'ssh {tgt_cred} "sudo su pi; . /opt/alfa_cr6/venv/bin/activate ; alfa_CR6 -display :0 &"',
     ]
 
     for cmd_ in cmds:
@@ -109,7 +119,9 @@ def main():
     logging.basicConfig(stream=sys.stdout, level=args.log_level, format=fmt_)
 
     logging.info(f"args:{args}")
-    logging.warning(f"see log in {PROJECT_ROOT}/make.log")
+    logging.warning(f"see also command log msgs in {OUT_ERR_PTH}")
+    with open(OUT_ERR_PTH, 'w') as f: # reset messages
+        f.write(f'{time.asctime()}\n')
 
     if args.build:
         build(args)
@@ -117,6 +129,8 @@ def main():
         install_editable(args)
     if args.install_target:
         install_target(args)
+    if args.makedirs_on_target:
+        makedirs_on_target(args)
 
 
 main()
