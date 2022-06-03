42e84a52883cd2e55b5cb89e6bdf704b28e200ad
giovanni-angeli <giovanni.angeli6@gmail.com>
2022-06-03 10:33:09 +0200

    0.0.1rc117

--- src/alfa_CR6_test/http_client_sample.py
+++ src/alfa_CR6_test/http_client_sample.py
@@ -5,65 +5,76 @@
 import http.client
 import json
 
+""" to be reported on label:
+
+"brand",
+"quality",
+"colorCode",
+"quantity",
+"batchId",
+ 
+"""
 
 SAMPLE_FORMULA = {
-    "header": "SW CRx formula file",
-    "version": "0.0.1",
-    "system": "COINS",
-    "jobId": "XFER_TEST",
-    "meta": {
-        "Brand": "UC     UNDERCOAT",
-        "Region": "",
-        "Colour code": "GS905",
-        "Variant": "",
-        "Colour name": "GREY SHADE: LIGHT GREY",
-        "Second-name": "",
-        "Year": "/",
-        "Remark": "HSS 2",
-        "Quality": "Octobase Eco Plus",
-        "Undercoat": "",
-        "Pictograms": "",
-        "Mutation date": "16-06-2021",
-        "Quantity": "1,10 Litro",
-        "Cumulative": "No",
-        "extra_info": [
-            "Octoral Information Services - COINS",
-            "Data 19-10-2021 [11:56]",
-            "Overview: Formula",
-            "COV [gr/l]: 275,2",
-            "We strongly advise you to check the colour before use, ",
-            "for many reasons the car colour can be slightly different than the mixed colour."
-        ]
-    },
-    "ingredients": [
-        {"code": "W46",
-            "weight(g)": 69.9,
-            "description": "TN11-W0PR-J00M-1YWC"},
-        {"code": "W47",
-            "weight(g)": 44.4,
-            "description": "G721-F0HH-Q003-PCYT"},
-        {"code": "W48",
-            "weight(g)": 26.4,
-            "description": "1T11-X02J-500M-CP2G"},
-        {"code": "W50",
-            "weight(g)": 20.0,
-            "description": "QG21-Y0KQ-M00K-NDR0"},
-        {"code": "W909",
-            "weight(g)": 5.6,
-            "description": "W909"},
-        {"code": "W60",
-            "weight(g)": 0.9,
-            "description": "-"},
-        {"code": "TW80",
-            "weight(g)": 19.5,
-            "description": "-"}
+  "header": "SW CRx formula file",
+  "version": "16.0.3.1",
+  "system": "Octoral Information Services",
+  "operatorId": "",
+  "jobId": "",
+  "batchId": 1857629189,
+  "action": None,
+  "meta": {
+    "brand": "UC",
+    "region": "",
+    "colorCode": "GIOVANNI_TEST",
+    "variantCode": "",
+    "colorName": "GREY SHADE: WHITE",
+    "secondName": "",
+    "year": "/",
+    "remark": "",
+    "quality": "DeBeer900",
+    "undercoat": "",
+    "pictograms": "180,180,180,180,180",
+    "dateModified": "16/06/2021",
+    "quantity": 0.0,
+    "cumulative": 0.0,
+    "extraInfo": [
+      "UC_GS903__GREY SHADE: WHITE",
+      ""
     ]
+  },
+  "ingredients": [
+    {
+      "code": "W10",
+      "weight(g)": 116.6687740407977,
+      "tolerance": 0,
+      "description": None
+    },
+    {
+      "code": "W98",
+      "weight(g)": 0.4907623708028845,
+      "tolerance": 0,
+      "description": None
+    },
+    {
+      "code": "W88",
+      "weight(g)": 0.24772933071628858,
+      "tolerance": 0,
+      "description": None
+    },
+    {
+      "code": "TW80",
+      "weight(g)": 9.770000145584346,
+      "tolerance": 0,
+      "description": None
+    }
+  ]
 }
-
 
 def main():
 
     host = '127.0.0.1'
+    host = '192.168.12.122'
     port = 8090
     method='POST'
     url='/upload_json_formula'
