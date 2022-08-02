ec16ee0eca02c3cd5b730326b455488ecc68d21e
giovanni-angeli <giovanni.angeli6@gmail.com>
2022-08-02 16:17:59 +0200

    add a sample formula in test/http_api_client.py

--- src/alfa_CR6_test/http_api_client.py
+++ src/alfa_CR6_test/http_api_client.py
@@ -13,7 +13,7 @@ import http.client
 import requests
 
 HOST = '127.0.0.1'
-# ~ HOST = '93.147.171.2'
+# ~ '93 147 171 2'
 # ~ HOST = '192.168.0.100'
 # ~ HOST = '192.168.12.122'
 
@@ -31,7 +31,7 @@ HOST = '127.0.0.1'
 
 URL_PREFIX = '/api/v1'
 
-SAMLPE_FILTER = [{"name":"description","op":"==","val":"1376848525"}]
+SAMLPE_FILTER = [{"name": "description", "op": "==", "val": "1376848525"}]
 
 
 def test_restless(resource_name):
@@ -79,7 +79,7 @@ def ask_orders_by_job_id(job_id):
     resp_str = response.read().decode()
     resp_dict = json.loads(resp_str)
     # ~ for d in resp_dict['data']:
-        # ~ d["json_properties"] = json.loads(d["json_properties"])
+    # ~ d["json_properties"] = json.loads(d["json_properties"])
     resp = json.dumps(resp_dict, indent=2, ensure_ascii=False)
     print(resp)
 
@@ -90,139 +90,200 @@ def upload_formula_file(file_path):
     url = f"http://{HOST}:{port}/upload_formula_file"
     print(f"url:{url}")
 
-    with open(file_path,'rb') as fd:
+    with open(file_path, 'rb') as fd:
         files = {'file': (os.path.basename(file_path), fd, 'application/json')}
         r = requests.post(url, files=files)
 
         print(f"r:{r}")
         # ~ print(r.prepare().body.decode('ascii'))
 
+
 def upload_json_formula():
 
-    SAMPLE_FORMULA = {
-      "header": "SW CRx formula file",
-      "version": "16.0.3.1",
-      "system": "Octoral Information Services",
-      "operatorId": "",
-      "jobId": "",
-      "batchId": random.randint(0, 2000000000),
-      "action": None,
-      "meta": {
-        "brand": "UC",
-        "region": "",
-        "colorCode": f"alfaTest-{random.randint(1, 10)}",
-        "variantCode": "",
-        "colorName": "GREY SHADE: WHITE",
-        "secondName": "",
-        "year": "/",
-        "remark": "",
-        "quality": "DeBeer900",
-        "undercoat": "",
-        "pictograms": "180,180,180,180,180",
-        "dateModified": "16/06/2021",
-        "quantity": 500.0,
-        "cumulative": 0.0,
-        "extraInfo": [
-          "UC_GS903__GREY SHADE: WHITE",
-          ""
-        ]
-      },
-      "ingredients": [
+    SAMPLE_FORMULAS = [
         {
-          "code": "W10",
-          "weight(g)": 116.6687740407977,
-          "tolerance": 0,
-          "description": None
+            "header": "SW CRx formula file",
+            "version": "16.0.3.1",
+            "system": "Octoral Information Services",
+            "operatorId": "",
+            "jobId": "",
+            "batchId": random.randint(0, 2000000000),
+            "action": None,
+            "meta": {
+                "brand": "UC",
+                "region": "",
+                "colorCode": f"alfaTest-{random.randint(1, 10)}",
+                "variantCode": "",
+                "colorName": "GREY SHADE: WHITE",
+                "secondName": "",
+                "year": "/",
+                "remark": "",
+                "quality": "DeBeer900",
+                "undercoat": "",
+                "pictograms": "180,180,180,180,180",
+                "dateModified": "16/06/2021",
+                "quantity": 500.0,
+                "cumulative": 0.0,
+                "extraInfo": [
+                    "UC_GS903__GREY SHADE: WHITE",
+                    ""
+                ]
+            },
+            "ingredients": [
+                {
+                    "code": "W10",
+                    "weight(g)": 116.6687740407977,
+                    "tolerance": 0,
+                    "description": None
+                },
+                {
+                    "code": "W11",
+                    "weight(g)": 116.6687740407977,
+                    "tolerance": 0,
+                    "description": None
+                },
+                # ~ {
+                # ~ "code": "W12",
+                # ~ "weight(g)": 116.6687740407977,
+                # ~ "tolerance": 0,
+                # ~ "description": None
+                # ~ },
+                {
+                    "code": "W98",
+                    "weight(g)": 0.4907623708028845,
+                    "tolerance": 0,
+                    "description": None
+                },
+                {
+                    "code": "W88",
+                    "weight(g)": 0.24772933071628858,
+                    "tolerance": 0,
+                    "description": None
+                },
+                {
+                    "code": "BW88",
+                    "weight(g)": 0.24772933071628858,
+                    "tolerance": 0,
+                    "description": None
+                },
+                {
+                    "code": "BW89",
+                    "weight(g)": 0.24772933071628858,
+                    "tolerance": 0,
+                    "description": None
+                },
+                {
+                    "code": "BW90",
+                    "weight(g)": 0.24772933071628858,
+                    "tolerance": 0,
+                    "description": None
+                },
+                {
+                    "code": "BW91",
+                    "weight(g)": 0.24772933071628858,
+                    "tolerance": 0,
+                    "description": None
+                },
+                {
+                    "code": "BW92",
+                    "weight(g)": 0.24772933071628858,
+                    "tolerance": 0,
+                    "description": None
+                },
+                {
+                    "code": "BW93",
+                    "weight(g)": 0.24772933071628858,
+                    "tolerance": 0,
+                    "description": None
+                },
+                {
+                    "code": "BW94",
+                    "weight(g)": 0.24772933071628858,
+                    "tolerance": 0,
+                    "description": None
+                },
+                {
+                    "code": "TW80",
+                    "weight(g)": 9.770000145584346,
+                    "tolerance": 0,
+                    "description": None
+                }
+            ]
         },
         {
-          "code": "W11",
-          "weight(g)": 116.6687740407977,
-          "tolerance": 0,
-          "description": None
-        },
-        # ~ {
-          # ~ "code": "W12",
-          # ~ "weight(g)": 116.6687740407977,
-          # ~ "tolerance": 0,
-          # ~ "description": None
-        # ~ },
-        {
-          "code": "W98",
-          "weight(g)": 0.4907623708028845,
-          "tolerance": 0,
-          "description": None
-        },
-        {
-          "code": "W88",
-          "weight(g)": 0.24772933071628858,
-          "tolerance": 0,
-          "description": None
-        },
-        {
-          "code": "BW88",
-          "weight(g)": 0.24772933071628858,
-          "tolerance": 0,
-          "description": None
-        },
-        {
-          "code": "BW89",
-          "weight(g)": 0.24772933071628858,
-          "tolerance": 0,
-          "description": None
-        },
-        {
-          "code": "BW90",
-          "weight(g)": 0.24772933071628858,
-          "tolerance": 0,
-          "description": None
-        },
-        {
-          "code": "BW91",
-          "weight(g)": 0.24772933071628858,
-          "tolerance": 0,
-          "description": None
-        },
-        {
-          "code": "BW92",
-          "weight(g)": 0.24772933071628858,
-          "tolerance": 0,
-          "description": None
-        },
-        {
-          "code": "BW93",
-          "weight(g)": 0.24772933071628858,
-          "tolerance": 0,
-          "description": None
-        },
-        {
-          "code": "BW94",
-          "weight(g)": 0.24772933071628858,
-          "tolerance": 0,
-          "description": None
-        },
-        {
-          "code": "TW80",
-          "weight(g)": 9.770000145584346,
-          "tolerance": 0,
-          "description": None
+            "header": "SW CRx formula file",
+            "version": "1.0.0",
+            "system": "OCTORAL",
+            "operatorId": "",
+            "jobId": "",
+            "batchId": random.randint(0, 2000000000),
+            "action": None,
+            "meta": {
+                "brand": "FORD",
+                "region": "",
+                "colorCode": "724",
+                "variantCode": "",
+                "colorName": " MIDNIGHT SKY PEARL",
+                "secondName": "GRIS METEORE PEARL",
+                "year": "2011/",
+                "remark": "",
+                "quality": "Octobase Eco Plus",
+                "undercoat": "",
+                "pictograms": "",
+                "dateModified": "",
+                "quantity": 0.0,
+                "cumulative": 0.0,
+                "extraInfo": [
+                    ""
+                ]
+            },
+            "ingredients": [
+                {
+                    "code": "W89",
+                    "weight(g)": 1.0,
+                    "tolerance": 0,
+                    "description": ""
+                },
+                {
+                    "code": "W23",
+                    "weight(g)": 2.0001,
+                    "tolerance": 0,
+                    "description": ""
+                },
+                {
+                    "code": "W97",
+                    "weight(g)": 3.0,
+                    "tolerance": 0,
+                    "description": ""
+                },
+                {
+                    "code": "W00",
+                    "weight(g)": 4.0001,
+                    "tolerance": 0,
+                    "description": ""
+                }
+            ]
         }
-      ]
-    }
+    ]
 
     host = HOST
     port = 8090
     method = 'POST'
     url = '/upload_json_formula'
-    json_data_as_string = json.dumps(SAMPLE_FORMULA)
+    for sample_formula in SAMPLE_FORMULAS:
+        json_data_as_string = json.dumps(sample_formula)
 
-    hdrs = {'Content-type': 'application/json'}
+        hdrs = {'Content-type': 'application/json'}
 
-    conn = http.client.HTTPConnection(host=host, port=port, timeout=5)
-    # ~ print(f"conn:{conn}, host:port={host}:{port}")
-    conn.request(method=method, url=url, body=json_data_as_string, headers=hdrs)
-    response = conn.getresponse()
+        conn = http.client.HTTPConnection(host=host, port=port, timeout=5)
+        # ~ print(f"conn:{conn}, host:port={host}:{port}")
+        conn.request(method=method, url=url, body=json_data_as_string, headers=hdrs)
+        response = conn.getresponse()
 
-    print(response.read().decode())
+        _resp = response.read().decode()
+        _resp = json.loads(_resp)
+
+        logging.warning(f"_resp:{_resp}")
 
 
 def main():
