e8c115507750b6d969d1f5502acc72aa298ba215
giovanni-angeli <giovanni.angeli6@gmail.com>
2023-02-22 17:34:41 +0100

    extend 'parse_axalta_pdf()' to cope with 'Cromax', 'Standox', 'Axalta Industrial'

--- src/alfa_CR6_backend/order_parser.py
+++ src/alfa_CR6_backend/order_parser.py
@@ -70,7 +70,7 @@ class OrderParser:       # pylint: disable=too-many-public-methods
     cpl_pdf_header = "MixingSys"
     DICHEMIX_pdf_header = "Dichemix"
     mixcar_pdf_header = "Rapport de formule"
-    axalta_pdf_header = "Axalta Industrial"         # duthoo
+    axalta_pdf_headers = ["Axalta Industrial", "Cromax", "Standox"] # duthoo, Gteam, Serwind/Autocolor
     codevid_pdf_header = "Formula Details"          # duthoo
     basf_1_pdf_header = "Base Target Amt. CumAmt."  # BASF Refinty
     basf_2_pdf_header = "Base Amount"               # BASF Cosima
@@ -641,6 +641,66 @@ weight:{RealWeight}
         def parse_ingredient_line(l):
 
             ingredient = None
+            l = l.replace(',', '.')
+            toks = l.split()
+            logging.error(f"toks:{toks}")
+            if len(toks) >= 5 and toks[0].isdigit() and toks[-1].replace('.','',1).isdigit()  and toks[-2].replace('.','',1).isdigit():
+
+                value = float(toks[-2])
+                name = toks[1]
+                description = " ".join(toks[2:-2])
+
+                ingredient = {
+                    "pigment_name": name,
+                    "weight(g)": round(value, 4),
+                    "description": description
+                }
+
+            logging.error(f"toks:{toks}, ingredient:{ingredient}")
+
+            return ingredient
+
+        properties = {}
+        ingredients = []
+        extra_info = []
+        extra_lines_to_print = []
+
+        for l in lines:
+
+            if not l:
+                continue
+
+            l = " ".join(l.split())
+
+            ingredient = parse_ingredient_line(l)
+            if ingredient:
+                ingredients.append(ingredient)
+            else:
+
+                if extra_info and [k for k in ["register", "codice colore produttore"] if k in extra_info[-1].lower()]:
+                    toks = l.split()
+                    if toks:
+                        extra_lines_to_print.append(" ".join(toks[:-1]))
+
+                elif extra_info and [k for k in ["kleurcode fabrikant", "nome colore produttore" ] if k in extra_info[-1].lower()]:
+                    toks = l.split()
+                    if toks:
+                        extra_lines_to_print.append(" ".join(toks[:-2]))
+
+                extra_info.append(l)
+
+        properties['extra_lines_to_print'] = extra_lines_to_print
+        properties['ingredients'] = ingredients
+        properties['meta'] = {'extra_info': extra_info}
+
+        return properties
+
+    @staticmethod
+    def parse_axalta_pdf_OLD(lines):  # pylint: disable=too-many-locals, too-many-branches, too-many-statements
+
+        def parse_ingredient_line(l):
+
+            ingredient = None
             toks = l.split()
             if len(toks) > 2:
 
@@ -969,7 +1029,6 @@ weight:{RealWeight}
         properties = {}
         ingredients = []
         extra_info = []
-        extra_lines_to_print = []
 
         for l in original_lines:
 
@@ -1038,7 +1097,6 @@ weight:{RealWeight}
         properties = {}
         ingredients = []
         extra_info = []
-        extra_lines_to_print = []
 
         for l in original_lines:
 
@@ -1134,11 +1192,12 @@ weight:{RealWeight}
             if properties.get('meta'):
                 properties['meta']['header'] = cls.mixcar_pdf_header
 
-        elif cls.axalta_pdf_header in " ".join(lines[0:4]):
+        elif [h for h in cls.axalta_pdf_headers if h in " ".join(lines[0:10])]:
+
             properties = cls.parse_axalta_pdf(lines)
 
             if properties.get('meta'):
-                properties['meta']['header'] = cls.axalta_pdf_header
+                properties['meta']['header'] = [h for h in cls.axalta_pdf_headers if h in " ".join(lines[0:10])][0]
 
         elif cls.codevid_pdf_header in " ".join(lines[0:1]):
             properties = cls.parse_codevid_pdf(original_lines)
