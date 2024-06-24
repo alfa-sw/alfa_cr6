4467f5e7daecf683016736bf258767037dfc8668
FrancescoGalasso <francescogalasso@alfadispenser.com>
2024-06-24 09:22:59 +0200

    Remove leftover conditional check `if hasattr(self.restore_machine_helper, 'store_jar_data')` from PR #142

--- src/alfa_CR6_backend/base_application.py
+++ src/alfa_CR6_backend/base_application.py
@@ -1178,9 +1178,6 @@ class BaseApplication(QApplication):  # pylint:  disable=too-many-instance-attri
 
                 self.db_session.commit()
 
-                if hasattr(self.restore_machine_helper, 'store_jar_data'):
-                    self.restore_machine_helper.store_jar_data(jar, pos)
-
                 if jar.status in {"ERROR", "DONE"}:
                     jar_data = jar.object_to_dict(include_relationship=2)
                     self.redis_publisher.publish_messages(jar_data)
