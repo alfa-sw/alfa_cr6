3b9f445fc5e473d3434912a8dba7d8f2c9aa8e4c
Filippo Gurgoglione <ziofil@gmail.com>
2024-06-26 14:28:11 +0200

    pre_load_libGLX_on_banana() called only when not executing in docker

--- src/alfa_CR6_backend/main.py
+++ src/alfa_CR6_backend/main.py
@@ -7,6 +7,7 @@
 import sys
 import logging
 import platform
+import os
 
 from alfa_CR6_backend.carousel_motor import CarouselMotor
 from alfa_CR6_backend.globals import import_settings
@@ -28,7 +29,8 @@ def main():
         stream=sys.stdout, level=settings.LOG_LEVEL,
         format="[%(asctime)s]%(levelname)s %(funcName)s() %(filename)s:%(lineno)d %(message)s")
 
-    pre_load_libGLX_on_banana()
+    if not os.getenv("IN_DOCKER", False) in ['1', 'true']:
+        pre_load_libGLX_on_banana()
 
     from alfa_CR6_frontend.main_window import MainWindow    # pylint: disable=import-outside-toplevel
     app = CarouselMotor(MainWindow, settings, sys.argv)
