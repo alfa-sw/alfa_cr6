0d81fba8e0508876e8900af132525ff322668b56
Filippo Gurgoglione <ziofil@gmail.com>
2024-06-26 14:27:51 +0200

    force logging settings while starting application

--- src/alfa_CR6_backend/main.py
+++ src/alfa_CR6_backend/main.py
@@ -24,9 +24,15 @@ def main():
 
     settings = import_settings()
 
-    logging.basicConfig(
-        stream=sys.stdout, level=settings.LOG_LEVEL,
-        format="[%(asctime)s]%(levelname)s %(funcName)s() %(filename)s:%(lineno)d %(message)s")
+    try:
+        logging.basicConfig(
+            force=True, level=settings.LOG_LEVEL,
+            format="[%(asctime)s]%(levelname)s %(funcName)s() %(filename)s:%(lineno)d %(message)s")
+    except ValueError:
+        # in case of old Python version
+        logging.basicConfig(
+            stream=sys.stdout, level=settings.LOG_LEVEL,
+            format="[%(asctime)s]%(levelname)s %(funcName)s() %(filename)s:%(lineno)d %(message)s")
 
     pre_load_libGLX_on_banana()
 
