fcfa646eb43b11cb0c6ada454af194bd2e8b86eb
FrancescoGalasso <francescogalasso@alfadispenser.com>
2025-04-01 16:35:02 +0200

    Remove jar from recovery data if detected too quickly

--- src/alfa_CR6_backend/carousel_motor.py
+++ src/alfa_CR6_backend/carousel_motor.py
@@ -425,6 +425,7 @@ class CarouselMotor(BaseApplication):  # pylint: disable=too-many-public-methods
                     self.double_can_alert = False
                     # ~ r = await _move_can_to_A()
                     asyncio.get_event_loop().call_later(.001, self.delete_entering_jar)
+                    await self.restore_machine_helper.async_remove_completed_jar_data(jar.barcode)
 
             self.busy_head_A = False
 
