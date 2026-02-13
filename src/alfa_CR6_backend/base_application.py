alfa-sw/alfa_cr6

commit bf35402258e1029d5899e71701b585fd0f59243c

minor improvement in avoiding the presence of 2 jars in HEAD 1 (#110)

src/alfa_CR6_backend/base_application.py
@@ -655,6 +655,10 @@ class BaseApplication(QApplication):  # pylint:  disable=too-many-instance-attri
                         self.main_window.open_alert_dialog(args, fmt=fmt, title="ERROR")
                         self.main_window.show_barcode(barcode, is_ok=False)
                     else:
+
+                        if self.carousel_frozen:
+                            logging.warning(f'carousel is frozen({carousel_frozen}) - returning from on_barcode_read ..')
+                            return
                         # let's run a task that will manage the jar through the entire path inside the system
                         t = self.__jar_task(barcode)
                         self.__jar_runners[barcode] = {

src/alfa_CR6_backend/carousel_motor.py
@@ -365,12 +365,16 @@ class CarouselMotor(BaseApplication):  # pylint: disable=too-many-public-methods
                 self.update_jar_position(jar=jar, machine_head=A, pos="IN_A")
                 await A.crx_outputs_management(1, 2)
                 await A.crx_outputs_management(0, 2)
-                r = await A.wait_for_jar_photocells_status("JAR_DISPENSING_POSITION_PHOTOCELL", on=True, timeout=13.1)
+                r = await A.wait_for_jar_photocells_status(
+                    "JAR_DISPENSING_POSITION_PHOTOCELL",
+                    on=True, timeout=13.1, show_alert=False)
                 await A.crx_outputs_management(1, 0)
                 await A.crx_outputs_management(0, 0)
             else:
                 await A.crx_outputs_management(0, 2)
-                r = await A.wait_for_jar_photocells_status("JAR_DISPENSING_POSITION_PHOTOCELL", on=True, timeout=13.2)
+                r = await A.wait_for_jar_photocells_status(
+                    "JAR_DISPENSING_POSITION_PHOTOCELL",
+                    on=True, timeout=13.2, show_alert=False)
                 await A.crx_outputs_management(0, 0)
 
             if r:

commit e7beefd1abaa5dc56e9dbb592890b63783e531ac

fix: restore ready_to_read_a_barcode flag when carousel is frozen in on_barcode_read
Prevents barcode reading from being permanently blocked after a carousel
freeze/unfreeze cycle. Bug introduced in bf35402 (v0.0.1rc179, Dec 2023).

src/alfa_CR6_backend/base_application.py
@@ -1003,6 +1003,7 @@ class BaseApplication(QApplication):  # pylint:  disable=too-many-instance-attri
 
                         if self.carousel_frozen:
                             logging.warning(f'carousel is frozen({self.carousel_frozen}) - returning from on_barcode_read ..')
+                            self.ready_to_read_a_barcode = True
                             return
 
 