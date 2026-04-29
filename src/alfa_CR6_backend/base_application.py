alfa-sw/alfa_cr6

commit f959eb18eb380d7fc5ab91eacc727e9089fbc64d

add in-transit jar labels and blinking error step on carousel move failure

src/alfa_CR6_backend/carousel_motor.py
@@ -940,8 +940,11 @@ class CarouselMotor(BaseApplication):  # pylint: disable=too-many-public-methods
                 r = await step(jar)
 
                 if not r:
+                    _step_key = _method_name.replace("move_", "") if _method_name else None
+
                     if "move_01_02" in _tag:
 
+                        self.main_window.start_step_blink(_step_key)
                         while True:
                             await self.wait_for_carousel_not_frozen(
                                 True,
@@ -957,6 +960,7 @@ class CarouselMotor(BaseApplication):  # pylint: disable=too-many-public-methods
                                 not _error_head.jar_photocells_status.get('JAR_DISPENSING_POSITION_PHOTOCELL', False)
                             ):
                                 break
+                        self.main_window.stop_step_blink()
 
                         self.delete_entering_jar()
 
@@ -977,6 +981,7 @@ class CarouselMotor(BaseApplication):  # pylint: disable=too-many-public-methods
                         "barcode:{} error in {}. I will retry.",
                         "({})"
                     ]
+                    self.main_window.start_step_blink(_step_key)
                     await self.wait_for_carousel_not_frozen(
                         True,
                         message_args=(barcode_, f"\n{_tag}\n", str(retry_counter),),
@@ -984,6 +989,7 @@ class CarouselMotor(BaseApplication):  # pylint: disable=too-many-public-methods
                         error_head=_error_head,
                         dest_head=_dst_head
                     )
+                    self.main_window.stop_step_blink()
 
                 else:
                     break

src/alfa_CR6_frontend/home_page.py
@@ -17,7 +17,7 @@ import traceback
 import asyncio
 import json
 
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
 
 from PyQt5.QtGui import QMovie
 from PyQt5.QtWidgets import QApplication
@@ -536,23 +536,49 @@ class RefillProcedureHelper:
 
 class HomePage(BaseStackedPage):
 
+    # in-transit labels: shown when adjacent photocells are simultaneously occupied
+    # subclasses declare = None for labels missing from their UI file
+    STEP_01_02_label = None
+
+    _blink_step_label = None
+    _blink_state = False
+    STEP_02_03_label = None
+    STEP_02_04_label = None  # four-heads only (skips HEAD B)
+    STEP_03_04_label = None
+    STEP_04_05_label = None
+    STEP_06_07_label = None
+    STEP_07_08_label = None
+    STEP_07_09_label = None  # four-heads only (skips HEAD E)
+    STEP_08_09_label = None
+    STEP_09_10_label = None
+
     def __init__(self, *args, **kwargs):  # pylint:disable=too-many-branches, too-many-statements
 
         super().__init__(*args, **kwargs)
 
         self.jar_pixmap_map = [
-            (self.STEP_01_label, (("A", "JAR_INPUT_ROLLER_PHOTOCELL"),), "IN_A",),
-            (self.STEP_02_label, (("A", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "A",),
-            (self.STEP_03_label, (("B", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "B",),
-            (self.STEP_04_label, (("C", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "C",),
-            (self.STEP_05_label, (("D", "LOAD_LIFTER_UP_PHOTOCELL"), ("C", "JAR_LOAD_LIFTER_ROLLER_PHOTOCELL"),), "LIFTR_UP",),
-            (self.STEP_06_label, (("D", "LOAD_LIFTER_DOWN_PHOTOCELL"), ("C", "JAR_LOAD_LIFTER_ROLLER_PHOTOCELL"),), "LIFTR_DOWN",),
-            (self.STEP_07_label, (("D", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "D",),
-            (self.STEP_08_label, (("E", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "E",),
-            (self.STEP_09_label, (("F", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "F",),
-            (self.STEP_10_label, (("F", "UNLOAD_LIFTER_DOWN_PHOTOCELL"), ("F", "JAR_UNLOAD_LIFTER_ROLLER_PHOTOCELL"),), "LIFTL_DOWN",),
-            (self.STEP_11_label, (("F", "UNLOAD_LIFTER_UP_PHOTOCELL"), ("F", "JAR_UNLOAD_LIFTER_ROLLER_PHOTOCELL"),), "LIFTL_UP",),
-            (self.STEP_12_label, (("F", "JAR_OUTPUT_ROLLER_PHOTOCELL"),), "OUT",),
+            (self.STEP_01_label,    (("A", "JAR_INPUT_ROLLER_PHOTOCELL"),), "IN_A",),
+            (self.STEP_01_02_label, (("A", "JAR_INPUT_ROLLER_PHOTOCELL"), ("A", "JAR_DISPENSING_POSITION_PHOTOCELL")), "IN_A",),
+            (self.STEP_02_label,    (("A", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "A",),
+            (self.STEP_02_03_label, (("A", "JAR_DISPENSING_POSITION_PHOTOCELL"), ("B", "JAR_DISPENSING_POSITION_PHOTOCELL")), "A",),
+            (self.STEP_02_04_label, (("A", "JAR_DISPENSING_POSITION_PHOTOCELL"), ("C", "JAR_DISPENSING_POSITION_PHOTOCELL")), "A",),
+            (self.STEP_03_label,    (("B", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "B",),
+            (self.STEP_03_04_label, (("B", "JAR_DISPENSING_POSITION_PHOTOCELL"), ("C", "JAR_DISPENSING_POSITION_PHOTOCELL")), "B",),
+            (self.STEP_04_label,    (("C", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "C",),
+            (self.STEP_04_05_label, (("C", "JAR_DISPENSING_POSITION_PHOTOCELL"), ("C", "JAR_LOAD_LIFTER_ROLLER_PHOTOCELL")), "C",),
+            (self.STEP_05_label,    (("D", "LOAD_LIFTER_UP_PHOTOCELL"), ("C", "JAR_LOAD_LIFTER_ROLLER_PHOTOCELL"),), "LIFTR_UP",),
+            (self.STEP_06_label,    (("D", "LOAD_LIFTER_DOWN_PHOTOCELL"), ("C", "JAR_LOAD_LIFTER_ROLLER_PHOTOCELL"),), "LIFTR_DOWN",),
+            (self.STEP_06_07_label, (("C", "JAR_LOAD_LIFTER_ROLLER_PHOTOCELL"), ("D", "JAR_DISPENSING_POSITION_PHOTOCELL")), "LIFTR_DOWN",),
+            (self.STEP_07_label,    (("D", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "D",),
+            (self.STEP_07_08_label, (("D", "JAR_DISPENSING_POSITION_PHOTOCELL"), ("E", "JAR_DISPENSING_POSITION_PHOTOCELL")), "D",),
+            (self.STEP_07_09_label, (("D", "JAR_DISPENSING_POSITION_PHOTOCELL"), ("F", "JAR_DISPENSING_POSITION_PHOTOCELL")), "D",),
+            (self.STEP_08_label,    (("E", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "E",),
+            (self.STEP_08_09_label, (("E", "JAR_DISPENSING_POSITION_PHOTOCELL"), ("F", "JAR_DISPENSING_POSITION_PHOTOCELL")), "E",),
+            (self.STEP_09_label,    (("F", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "F",),
+            (self.STEP_09_10_label, (("F", "JAR_DISPENSING_POSITION_PHOTOCELL"), ("F", "JAR_UNLOAD_LIFTER_ROLLER_PHOTOCELL")), "F",),
+            (self.STEP_10_label,    (("F", "UNLOAD_LIFTER_DOWN_PHOTOCELL"), ("F", "JAR_UNLOAD_LIFTER_ROLLER_PHOTOCELL"),), "LIFTL_DOWN",),
+            (self.STEP_11_label,    (("F", "UNLOAD_LIFTER_UP_PHOTOCELL"), ("F", "JAR_UNLOAD_LIFTER_ROLLER_PHOTOCELL"),), "LIFTL_UP",),
+            (self.STEP_12_label,    (("F", "JAR_OUTPUT_ROLLER_PHOTOCELL"),), "OUT",),
         ]
 
         self.running_jars_lbl.setStyleSheet("font-size: 15px")
@@ -843,8 +869,35 @@ class HomePage(BaseStackedPage):
         self.running_jars_lbl.setText("\n".join(list_))
 
         for lbl, head_letters_bit_names, position in self.jar_pixmap_map:
-            if lbl:
+            if lbl and lbl is not self._blink_step_label:
                 self.__set_pixmap_by_photocells(lbl, head_letters_bit_names, position)
+
+    def start_step_blink(self, step_key):
+        lbl = getattr(self, f"STEP_{step_key}_label", None)
+        if lbl:
+            self._blink_step_label = lbl
+            self._blink_state = False
+            self._do_blink()
+
+    def stop_step_blink(self):
+        lbl = self._blink_step_label
+        self._blink_step_label = None
+        if lbl:
+            lbl.setStyleSheet("QLabel {}")
+            lbl.setText("")
+
+    def _do_blink(self):
+        if not self._blink_step_label:
+            return
+        self._blink_state = not self._blink_state
+        if self._blink_state:
+            _url = get_res("IMAGE", "jar-orange.png")
+            self._blink_step_label.setStyleSheet(
+                f'color:#000000; border-image:url("{_url}"); font-size: 15px')
+        else:
+            self._blink_step_label.setStyleSheet("QLabel {}")
+            self._blink_step_label.setText("")
+        QTimer.singleShot(500, self._do_blink)
 
     @staticmethod
     def __set_pixmap_by_photocells(  # pylint: disable=too-many-locals
@@ -1136,6 +1189,9 @@ class HomePageSixHeads(HomePage):
     ui_file_name = "home_page_six_heads.ui"
     help_file_name = 'home_six_heads.html'
 
+    STEP_02_04_label = None  # four-heads only
+    STEP_07_09_label = None  # four-heads only
+
 
 class HomePageFourHeads(HomePage):
 
@@ -1147,6 +1203,11 @@ class HomePageFourHeads(HomePage):
 
     STEP_03_label = None
     STEP_08_label = None
+
+    STEP_02_03_label = None  # six-heads only (HEAD B not present)
+    STEP_03_04_label = None  # six-heads only
+    STEP_07_08_label = None  # six-heads only (HEAD E not present)
+    STEP_08_09_label = None  # six-heads only
 
     refill_3_lbl = None
     refill_4_lbl = None
@@ -1212,6 +1273,7 @@ class HomePageCRX60Heads(HomePage):
 
         self.jar_pixmap_map = [
             (self.STEP_01_label, (("A", "JAR_INPUT_ROLLER_PHOTOCELL"),), "IN_A",),
+            (self.STEP_01_02_label, (("A", "JAR_INPUT_ROLLER_PHOTOCELL"), ("A", "JAR_DISPENSING_POSITION_PHOTOCELL")), "IN_A",),
             (self.STEP_02_label, (("A", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "A",),
             (self.STEP_03_label, (("B", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "B",),
             (self.STEP_04_label, (("C", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "C",),
@@ -1274,6 +1336,7 @@ class HomePageCRX40Heads(HomePage):
 
         self.jar_pixmap_map = [
             (self.STEP_01_label, (("A", "JAR_INPUT_ROLLER_PHOTOCELL"),), "IN_A",),
+            (self.STEP_01_02_label, (("A", "JAR_INPUT_ROLLER_PHOTOCELL"), ("A", "JAR_DISPENSING_POSITION_PHOTOCELL")), "IN_A",),
             (self.STEP_02_label, (("A", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "A",),
             (self.STEP_04_label, (("C", "JAR_DISPENSING_POSITION_PHOTOCELL"),), "C",),
             (self.STEP_05_label, (("C", "JAR_LOAD_LIFTER_ROLLER_PHOTOCELL"),), "OUT",),

src/alfa_CR6_frontend/main_window.py
@@ -712,6 +712,12 @@ class MainWindow(QMainWindow):  # pylint:  disable=too-many-instance-attributes
 
         self.home_page.show_reserve(head_index, flag=flag)
 
+    def start_step_blink(self, step_key):
+        self.home_page.start_step_blink(step_key)
+
+    def stop_step_blink(self):
+        self.home_page.stop_step_blink()
+
     def show_carousel_frozen(self, flag):
         if flag:
             self.home_page.freeze_carousel_btn.setText(tr_("Carousel Paused"))

src/alfa_CR6_frontend/ui/home_page_four_heads.ui
@@ -838,6 +838,27 @@
     <set>Qt::AlignCenter</set>
    </property>
   </widget>
+  <widget class="QLabel" name="STEP_01_02_label">
+   <property name="geometry">
+    <rect>
+     <x>670</x>
+     <y>370</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
   <widget class="QLabel" name="container_presence_1_label">
    <property name="geometry">
     <rect>
@@ -1288,6 +1309,111 @@
     </size>
    </property>
   </widget>
+  <widget class="QLabel" name="STEP_02_04_label">
+   <property name="geometry">
+    <rect>
+     <x>1050</x>
+     <y>370</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
+  <widget class="QLabel" name="STEP_04_05_label">
+   <property name="geometry">
+    <rect>
+     <x>1431</x>
+     <y>370</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
+  <widget class="QLabel" name="STEP_06_07_label">
+   <property name="geometry">
+    <rect>
+     <x>1422</x>
+     <y>740</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
+  <widget class="QLabel" name="STEP_07_09_label">
+   <property name="geometry">
+    <rect>
+     <x>1050</x>
+     <y>740</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
+  <widget class="QLabel" name="STEP_09_10_label">
+   <property name="geometry">
+    <rect>
+     <x>660</x>
+     <y>740</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
   <zorder>sinottico</zorder>
   <zorder>service_1_btn</zorder>
   <zorder>service_6_btn</zorder>
@@ -1316,6 +1442,7 @@
   <zorder>STEP_10_label</zorder>
   <zorder>STEP_02_label</zorder>
   <zorder>STEP_09_label</zorder>
+  <zorder>STEP_01_02_label</zorder>
   <zorder>STEP_01_label</zorder>
   <zorder>STEP_11_label</zorder>
   <zorder>STEP_12_label</zorder>
@@ -1344,11 +1471,16 @@
   <zorder>recovery_btn</zorder>
   <zorder>recovery_info_btn</zorder>
   <zorder>fw_align_ko_lbl</zorder>
+  <zorder>STEP_02_04_label</zorder>
+  <zorder>STEP_04_05_label</zorder>
+  <zorder>STEP_06_07_label</zorder>
+  <zorder>STEP_07_09_label</zorder>
+  <zorder>STEP_09_10_label</zorder>
  </widget>
  <resources/>
  <connections/>
  <buttongroups>
-  <buttongroup name="action_btn_group"/>
   <buttongroup name="service_btn_group"/>
+  <buttongroup name="action_btn_group"/>
  </buttongroups>
 </ui>

src/alfa_CR6_frontend/ui/home_page_six_heads.ui
@@ -1069,6 +1069,174 @@
     <set>Qt::AlignCenter</set>
    </property>
   </widget>
+  <widget class="QLabel" name="STEP_01_02_label">
+   <property name="geometry">
+    <rect>
+     <x>475</x>
+     <y>370</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
+  <widget class="QLabel" name="STEP_02_03_label">
+   <property name="geometry">
+    <rect>
+     <x>847</x>
+     <y>370</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
+  <widget class="QLabel" name="STEP_03_04_label">
+   <property name="geometry">
+    <rect>
+     <x>1235</x>
+     <y>370</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
+  <widget class="QLabel" name="STEP_04_05_label">
+   <property name="geometry">
+    <rect>
+     <x>1617</x>
+     <y>370</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
+  <widget class="QLabel" name="STEP_06_07_label">
+   <property name="geometry">
+    <rect>
+     <x>1615</x>
+     <y>740</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
+  <widget class="QLabel" name="STEP_07_08_label">
+   <property name="geometry">
+    <rect>
+     <x>1235</x>
+     <y>745</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
+  <widget class="QLabel" name="STEP_08_09_label">
+   <property name="geometry">
+    <rect>
+     <x>847</x>
+     <y>745</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
+  <widget class="QLabel" name="STEP_09_10_label">
+   <property name="geometry">
+    <rect>
+     <x>470</x>
+     <y>740</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
   <widget class="QLabel" name="container_presence_1_label">
    <property name="geometry">
     <rect>
@@ -1666,6 +1834,14 @@ RECOVERY</string>
   <zorder>STEP_03_label</zorder>
   <zorder>STEP_02_label</zorder>
   <zorder>STEP_09_label</zorder>
+  <zorder>STEP_01_02_label</zorder>
+  <zorder>STEP_02_03_label</zorder>
+  <zorder>STEP_03_04_label</zorder>
+  <zorder>STEP_04_05_label</zorder>
+  <zorder>STEP_06_07_label</zorder>
+  <zorder>STEP_07_08_label</zorder>
+  <zorder>STEP_08_09_label</zorder>
+  <zorder>STEP_09_10_label</zorder>
   <zorder>STEP_01_label</zorder>
   <zorder>STEP_08_label</zorder>
   <zorder>STEP_11_label</zorder>
@@ -1707,7 +1883,7 @@ RECOVERY</string>
  <resources/>
  <connections/>
  <buttongroups>
-  <buttongroup name="action_btn_group"/>
   <buttongroup name="service_btn_group"/>
+  <buttongroup name="action_btn_group"/>
  </buttongroups>
 </ui>

src/alfa_CR6_frontend/ui/home_page_three_heads.ui
@@ -567,6 +567,27 @@
     <set>Qt::AlignCenter</set>
    </property>
   </widget>
+  <widget class="QLabel" name="STEP_01_02_label">
+   <property name="geometry">
+    <rect>
+     <x>312</x>
+     <y>460</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
   <widget class="QLabel" name="container_presence_1_label">
    <property name="geometry">
     <rect>
@@ -930,6 +951,69 @@ RECOVERY</string>
     </size>
    </property>
   </widget>
+  <widget class="QLabel" name="STEP_02_03_label">
+   <property name="geometry">
+    <rect>
+     <x>748</x>
+     <y>460</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
+  <widget class="QLabel" name="STEP_03_04_label">
+   <property name="geometry">
+    <rect>
+     <x>1190</x>
+     <y>460</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
+  <widget class="QLabel" name="STEP_04_05_label">
+   <property name="geometry">
+    <rect>
+     <x>1630</x>
+     <y>460</y>
+     <width>85</width>
+     <height>85</height>
+    </rect>
+   </property>
+   <property name="font">
+    <font>
+     <pointsize>10</pointsize>
+    </font>
+   </property>
+   <property name="scaledContents">
+    <bool>true</bool>
+   </property>
+   <property name="alignment">
+    <set>Qt::AlignCenter</set>
+   </property>
+  </widget>
   <zorder>sinottico</zorder>
   <zorder>service_1_btn</zorder>
   <zorder>service_3_btn</zorder>
@@ -949,6 +1033,7 @@ RECOVERY</string>
   <zorder>STEP_05_label</zorder>
   <zorder>STEP_03_label</zorder>
   <zorder>STEP_02_label</zorder>
+  <zorder>STEP_01_02_label</zorder>
   <zorder>STEP_01_label</zorder>
   <zorder>container_presence_1_label</zorder>
   <zorder>container_presence_3_label</zorder>
@@ -970,11 +1055,14 @@ RECOVERY</string>
   <zorder>recovery_info_btn</zorder>
   <zorder>recovery_btn</zorder>
   <zorder>fw_align_ko_lbl</zorder>
+  <zorder>STEP_02_03_label</zorder>
+  <zorder>STEP_03_04_label</zorder>
+  <zorder>STEP_04_05_label</zorder>
  </widget>
  <resources/>
  <connections/>
  <buttongroups>
-  <buttongroup name="action_btn_group"/>
   <buttongroup name="service_btn_group"/>
+  <buttongroup name="action_btn_group"/>
  </buttongroups>
 </ui>

commit bde6a9f877ee9490379da90460701c0aef8a32ea

fix: stop in-transit jar blink on task cancel from debug page
wrap blink-active waits in carousel_motor.move_jar with try/finally so
stop_step_blink runs on CancelledError; also call stop_step_blink in
delete_jar_runner / _del_entering_jar as defense-in-depth.
bug present since v1.9.0 (introduced by f959eb1).

src/alfa_CR6_backend/base_application.py
@@ -1515,6 +1515,8 @@ class BaseApplication(QApplication):  # pylint:  disable=too-many-instance-attri
         r = entering_jar["task"].cancel()
         logging.warning(f"cancelled. r:{r}")
 
+        self.main_window.stop_step_blink()
+
         logging.warning(f"deleting:{entering_jar}")
         del entering_jar
         logging.warning(f"deleted:{kode}")
@@ -1560,6 +1562,8 @@ class BaseApplication(QApplication):  # pylint:  disable=too-many-instance-attri
                 logging.warning(f'cancelling:{j["task"]}')
                 r = j["task"].cancel()
                 logging.warning(f"cancelled. r:{r}")
+
+                self.main_window.stop_step_blink()
 
                 logging.warning(f"deleting:{j}")
                 del j

src/alfa_CR6_backend/carousel_motor.py
@@ -1006,22 +1006,24 @@ class CarouselMotor(BaseApplication):  # pylint: disable=too-many-public-methods
                     if "move_01_02" in _tag:
 
                         self.main_window.start_step_blink(_step_key)
-                        while True:
-                            await self.wait_for_carousel_not_frozen(
-                                True,
-                                message_args=(barcode_, f"\n{_tag}\n"),
-                                message_fmt='barcode:{} error in {}. Remove all Cans from input roller and from HEAD A!',
-                                visibility=2,
-                                show_cancel_btn=False,
-                                error_head=_error_head,
-                                dest_head=_dst_head
-                            )
-                            if not _error_head or (
-                                not _error_head.jar_photocells_status.get('JAR_INPUT_ROLLER_PHOTOCELL', False) and
-                                not _error_head.jar_photocells_status.get('JAR_DISPENSING_POSITION_PHOTOCELL', False)
-                            ):
-                                break
-                        self.main_window.stop_step_blink()
+                        try:
+                            while True:
+                                await self.wait_for_carousel_not_frozen(
+                                    True,
+                                    message_args=(barcode_, f"\n{_tag}\n"),
+                                    message_fmt='barcode:{} error in {}. Remove all Cans from input roller and from HEAD A!',
+                                    visibility=2,
+                                    show_cancel_btn=False,
+                                    error_head=_error_head,
+                                    dest_head=_dst_head
+                                )
+                                if not _error_head or (
+                                    not _error_head.jar_photocells_status.get('JAR_INPUT_ROLLER_PHOTOCELL', False) and
+                                    not _error_head.jar_photocells_status.get('JAR_DISPENSING_POSITION_PHOTOCELL', False)
+                                ):
+                                    break
+                        finally:
+                            self.main_window.stop_step_blink()
 
                         self.delete_entering_jar()
 
@@ -1049,14 +1051,16 @@ class CarouselMotor(BaseApplication):  # pylint: disable=too-many-public-methods
                     )
                     if not _is_too_many_jars:
                         self.main_window.start_step_blink(_step_key)
-                    await self.wait_for_carousel_not_frozen(
-                        True,
-                        message_args=(barcode_, f"\n{_tag}\n", str(retry_counter),),
-                        message_fmt=msg_,
-                        error_head=_error_head,
-                        dest_head=_dst_head
-                    )
-                    self.main_window.stop_step_blink()
+                    try:
+                        await self.wait_for_carousel_not_frozen(
+                            True,
+                            message_args=(barcode_, f"\n{_tag}\n", str(retry_counter),),
+                            message_fmt=msg_,
+                            error_head=_error_head,
+                            dest_head=_dst_head
+                        )
+                    finally:
+                        self.main_window.stop_step_blink()
 
                 else:
                     break